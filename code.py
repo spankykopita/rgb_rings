835df978195bc35e6c210396df5281909c4c896b
spankykopita <canasclone@gmail.com>
2023-01-25 23:26:12 -0800

    Switch example to adafruit rainbow cycling

--- code.py
+++ code.py
@@ -1,86 +1,38 @@
-# Gemma IO demo
-# Welcome to CircuitPython 5 :)
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
 
-import usb_hid
-from adafruit_hid.keyboard import Keyboard
-from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
-from adafruit_hid.keycode import Keycode
-from digitalio import DigitalInOut, Direction, Pull
-from analogio import AnalogIn, AnalogOut
-from touchio import TouchIn
-import adafruit_dotstar as dotstar
-import microcontroller
+""" Simple FancyLED example for NeoPixel strip
+"""
+
 import board
-import time
+import neopixel
+import adafruit_fancyled.adafruit_fancyled as fancy
 
-# One pixel connected internally!
-dot = dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1, brightness=0.2)
+num_leds = 32
 
-# Built in red LED
-led = DigitalInOut(board.D13)
-led.direction = Direction.OUTPUT
+# Declare a 6-element RGB rainbow palette
+palette = [
+    fancy.CRGB(1.0, 0.0, 0.0),  # Red
+    fancy.CRGB(0.5, 0.5, 0.0),  # Yellow
+    fancy.CRGB(0.0, 1.0, 0.0),  # Green
+    fancy.CRGB(0.0, 0.5, 0.5),  # Cyan
+    fancy.CRGB(0.0, 0.0, 1.0),  # Blue
+    fancy.CRGB(0.5, 0.0, 0.5),
+]  # Magenta
 
-# Analog output on A0
-aout = AnalogOut(board.A0)
+# Declare a NeoPixel object on pin D6 with num_leds pixels, no auto-write.
+# Set brightness to max because we'll be using FancyLED's brightness control.
+pixels = neopixel.NeoPixel(board.D1, num_leds, brightness=1.0, auto_write=False)
 
-# Analog input on A1
-analog1in = AnalogIn(board.A1)
+offset = 0  # Positional offset into color palette to get it to 'spin'
 
-# Capacitive touch on A2
-touch2 = TouchIn(board.A2)
-
-# The keyboard object! Used if we do HID output, see below
-time.sleep(1)  # Sleep for a bit to avoid a race condition on some systems
-keyboard = Keyboard(usb_hid.devices)
-keyboard_layout = KeyboardLayoutUS(keyboard)  # We're in the US :)
-
-######################### HELPERS ##############################
-
-# Helper to convert analog input to voltage
-def getVoltage(pin):
-    return (pin.value * 3.3) / 65536
-
-# Helper to give us a nice color swirl
-def wheel(pos):
-    # Input a value 0 to 255 to get a color value.
-    # The colours are a transition r - g - b - back to r.
-    if (pos < 0):
-        return [0, 0, 0]
-    if (pos > 255):
-        return [0, 0, 0]
-    if (pos < 85):
-        return [int(pos * 3), int(255 - (pos*3)), 0]
-    elif (pos < 170):
-        pos -= 85
-        return [int(255 - pos*3), 0, int(pos*3)]
-    else:
-        pos -= 170
-        return [0, int(pos*3), int(255 - pos*3)]
-
-######################### MAIN LOOP ##############################
-
-i = 0
 while True:
-  # spin internal LED around!
-  dot[0] = wheel(i)
-  dot.show()
+    for i in range(num_leds):
+        # Load each pixel's color from the palette using an offset, run it
+        # through the gamma function, pack RGB value and assign to pixel.
+        color = fancy.palette_lookup(palette, offset + i / num_leds)
+        color = fancy.gamma_adjust(color, brightness=0.25)
+        pixels[i] = color.pack()
+    pixels.show()
 
-  # set analog output to 0-3.3V (0-65535 in increments)
-  aout.value = i * 256
-
-  # once every 256 ticks, so it doesnt rush by!
-  if i == 0:
-      # Read analog voltage on A1
-      print("A1: %0.2f" % getVoltage(analog1in))
-      # Print the temperature
-      print("Temp: %0.1f" % microcontroller.cpu.temperature)
-
-  # use A2 as capacitive touch to turn on internal LED
-  if touch2.value:
-        print("A2 touched!")
-        # optional! uncomment below & save to have it sent a keypress
-        #keyboard.press(Keycode.A)
-        #keyboard.release_all()
-  led.value = touch2.value
-
-  i = (i+1) % 256  # run from 0 to 255
+    offset += 0.02  # Bigger number = faster spin
